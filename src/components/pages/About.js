bbb89fe99a4cc096cef65f4b0955b5b7cf0a8b31
Gerardo Garcia-Salas <gergasa94@gmail.com>
2023-08-12 22:24:15 -0600

    functional portfolio

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,15 +2,10 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div id="About-Me" class="About-Me">
+      <h2>About Me</h2>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        I am a physician invested in continuing education. I intend to pursue a further honing in my current skills
       </p>
     </div>
   );
