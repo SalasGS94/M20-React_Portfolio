f9b0cae4177c11531d9a1d6d23d1bf79ecb7689f
Gerardo Garcia-Salas <gergasa94@gmail.com>
2023-08-14 23:25:35 -0600

    right navbar

---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Portfolio() {
+  return (
+    <div id="Work" class="Work">
+      <h2>Portfolio</h2>
+      <div class="container">
+        <a href="https://google.com/">
+          <div class="Google">
+            <h3>Google site</h3>
+          </div>
+        </a>
+        <a href="https://youtube.com/">
+          <div class="Youtube">
+            <h3>Youtube site</h3>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+}
